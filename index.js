b075b82d0f5b0b5888532ed96c6d11180a3f74c0
Giorgiana Bîrsan <64731577+GiorgianaBirsan@users.noreply.github.com>
2025-02-17 16:47:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser= require('body-parser');
+const { type } = require('express/lib/response');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+
+function stringIsAValidUrl(url) {
+
+  const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+  return urlRegex.test(url);
+}
+
+
+var short_url=0
+var urlsArray= {}
+
 app.use(cors());
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -21,20 +33,18 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-var short_url=0
-var urlsArray= {}
-
-function stringIsAValidUrl(url) {
-
-  const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
-  return urlRegex.test(url);
-}
-
 app.post('/api/shorturl', function(req, res) {
   const originalURL= req.body.url
   short_url++
-  urlsArray[short_url]= originalURL
-  res.json({ "original_url" : originalURL, "short_url" : short_url});
+  
+  if (!originalURL || !stringIsAValidUrl(originalURL)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  if(originalURL && typeof originalURL === 'string'){
+    urlsArray[`${short_url}`]= originalURL
+    res.json({ "original_url" : originalURL, "short_url" : short_url.toString()});
+  }
 });
 
 app.get('/api/shorturl/:short_url', function(req, res) {
