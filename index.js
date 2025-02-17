e742f66c5be38e6457e69715f5cefe226a1965d6
Giorgiana Bîrsan <64731577+GiorgianaBirsan@users.noreply.github.com>
2025-02-17 16:02:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser= require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded())
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +20,31 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+var short_url=0
+var urlsArray= {}
+
+function stringIsAValidUrl(url) {
+
+  const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+  return urlRegex.test(url);
+}
+
+app.post('/api/shorturl', function(req, res) {
+  const originalURL= req.body.url
+  short_url++
+  urlsArray[short_url]= originalURL
+  res.json({ "original_url" : originalURL, "short_url" : short_url});
+});
+
+app.get('/api/shorturl/:short_url', function(req, res) {
+     const short_url= req.params.short_url
+     if(stringIsAValidUrl(urlsArray[short_url])){
+       res.redirect(urlsArray[short_url])
+     }else{
+      res.json({error: 'invalid url'})
+     } 
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
